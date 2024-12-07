9b15f66c5c1ca526252c950bf23a14c5f73b19b2
rohan-talentica <rohan.gaonkar@talentica.com>
2024-12-07 18:16:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,53 +1,29 @@
-// Create a web server
-// Create a route for /comments
-// When the user visits /comments, return a list of comments
-// The comments should be an array of objects, each object should have an id and a comment key
-// The id should be a unique number
-// The comment should be a string
-// The comments should be saved in a file called comments.json
-// When the user visits /comments, read the comments from the file and return them
-
+//Create web server
 const express = require('express');
-const fs = require('fs');
-
 const app = express();
-app.use(express.json());
+const port = 3000;
 
+const comments = [
+  {
+    id: 1,
+    username: 'Alice',
+    comment: 'Hello from Alice'
+  },
+  {
+    id: 2,
+    username: 'Bob',
+    comment: 'Hello from Bob'
+  },
+  {
+    id: 3,
+    username: 'Charlie',
+    comment: 'Hello from Charlie'
+  }
+];
 app.get('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.send([]);
-        } else {
-            res.send(JSON.parse(data));
-        }
-    });
+  res.json(comments);
 });
 
-app.post('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            const comments = [];
-            comments.push({
-                id: 1,
-                comment: req.body.comment
-            });
-            fs.writeFile('comments.json', JSON.stringify(comments), () => {
-                res.send(comments);
-            });
-        } else {
-            const comments = JSON.parse(data);
-            const newComment = {
-                id: comments.length + 1,
-                comment: req.body.comment
-            };
-            comments.push(newComment);
-            fs.writeFile('comments.json', JSON.stringify(comments), () => {
-                res.send(comments);
-            });
-        }
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
